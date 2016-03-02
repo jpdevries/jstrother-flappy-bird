b0fccaf9dc4fb5af7a835e2e424a40d212f847c6
Jim Strother <strotherwebdev@gmail.com>
2016-03-02 03:45:59 +0000

    finished animation

--- site/js/scripts.js
+++ site/js/scripts.js
@@ -1,10 +1,9 @@
 $(function() {
-    $('#bird')
-        .velocity({ translateY: "0vh" }, { duration: 10000 });
+    $('#bird').velocity({ translateY: "-100vh" },0,function(){
+          $('#bird').css({visibility:'visible'}).velocity({ translateY: "0" },{duration:1000, easing:'ease-out'});
+    });
     $('#howTo-button')
-        .css('display', 'inline')
         .velocity('transition.slideLeftIn', { duration: 2500 });
     $('#play-button')
-        .css('display', 'inline')
         .velocity('transition.slideRightIn', { duration: 2500 });
 });
