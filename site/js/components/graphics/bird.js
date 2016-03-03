8f904b219cfdc3f40bd62e2f90ab42eaeb55f488
Jim Strother <strotherwebdev@gmail.com>
2016-03-03 00:39:29 +0000

    beginning game structure

---
+++ site/js/components/graphics/bird.js
@@ -0,0 +1,6 @@
+var BirdGraphicsComponent = function(entity) {
+    this.entity = entity;
+};
+BirdGraphicsComponent.prototype.draw = function() {
+    console.log('Drawing A Bird');
+};
---
+++ site/js/entities/bird.js
@@ -0,0 +1,7 @@
+var Bird = function () {
+    console.log('Creating Bird Entity');
+    this.components = {
+        
+    };
+};
+exports.Bird = Bird;
