8e66a32a559859670acb297bd124f3f9453dfb38
Jim Strother <strotherwebdev@gmail.com>
2016-03-13 19:49:53 +0000

    adding input interaction

--- site/js/entities/bird.js
+++ site/js/entities/bird.js
@@ -1,8 +1,12 @@
 var graphicsComponent = require("../components/graphics/bird");
-
+var physicsComponent = require("../components/physics/physics");
 var Bird = function () {
+    var physics = new physicsComponent.PhysicsComponent(this);
+    physics.position.y = 0.5;
+    physics.acceleration.y = -2;
     var graphics = new graphicsComponent.BirdGraphicsComponent(this);
     this.components = {
+        physics: physics,
         graphics: graphics
     };
 };
---
+++ site/js/systems/input.js
@@ -0,0 +1,12 @@
+var InputSystem = function(entities) {
+    this.entities = entities;
+    this.canvas = document.getElementById('main-canvas');
+};
+InputSystem.prototype.run = function() {
+    this.canvas.addEventListener('click', this.onClick.bind(this));
+};
+InputSystem.prototype.onClick = function() {
+    var bird = this.entities[0];
+    bird.components.physics.velocity.y = 0.7;
+};
+exports.InputSystem = InputSystem;
