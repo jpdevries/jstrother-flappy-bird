18056370668198fb867fadba682d637259a1d01e
Jim Strother <strotherwebdev@gmail.com>
2016-03-13 19:52:35 +0000

    adjusted gravity

--- site/js/entities/bird.js
+++ site/js/entities/bird.js
@@ -3,7 +3,7 @@ var physicsComponent = require("../components/physics/physics");
 var Bird = function () {
     var physics = new physicsComponent.PhysicsComponent(this);
     physics.position.y = 0.5;
-    physics.acceleration.y = -2;
+    physics.acceleration.y = -1.5;
     var graphics = new graphicsComponent.BirdGraphicsComponent(this);
     this.components = {
         physics: physics,
